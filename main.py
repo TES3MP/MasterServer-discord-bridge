0cece05efc1767bde1e7977ad9f020ae8f6f34ff
Koncord <stas5978@gmail.com>
2017-09-21 01:24:45 +0800

    Add game status

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    await bot.change_presence(game=discord.Game(name=config.game_status))
 
 
 @bot.event
